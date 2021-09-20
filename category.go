d891b837ee0a87a113270251794637d0ce34b638
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-09-20 23:56:01 +0600

    commits

--- category.go
+++ category.go
@@ -1,15 +1,17 @@
 package category_nurzhas_store
 
 type Category struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	ImageUrl    string `json:"image_url"`
+	Id               string `json:"id"`
+	Name             string `json:"name"`
+	SmallDescription string `json:"small_description"`
+	BigDescription   string `json:"big_description"`
+	ImageUrl         string `json:"image_url"`
 }
 
 type CategoryUpdate struct {
-	Id          string  `json:"id"`
-	Name        *string `json:"name"`
-	Description *string `json:"description"`
-	ImageUrl    *string `json:"image_url"`
+	Id               string  `json:"id"`
+	Name             *string `json:"name"`
+	SmallDescription *string `json:"small_description"`
+	BigDescription   *string `json:"big_description"`
+	ImageUrl         *string `json:"image_url"`
 }
