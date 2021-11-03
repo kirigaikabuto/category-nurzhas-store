1601f0f571144e9955fd6f084b54400577bc470f
TLeugazy Yerassyl <tleugazy98@gmail.com>
2021-11-03 13:32:51 +0600

    commits

---
+++ order.go
@@ -0,0 +1,14 @@
+package category_nurzhas_store
+
+type Order struct {
+	BuildingType   string `json:"building_type"`
+	Width          string `json:"width"`
+	Height         string `json:"height"`
+	Length         string `json:"length"`
+	PanelType      string `json:"panel_type"`
+	InsulationType string `json:"insulation_type"`
+	PanelDepth     string `json:"panel_depth"`
+	LayoutType     string `json:"layout_type"`
+	PanelWidth     string `json:"panel_width"`
+	Color          string `json:"color"`
+}
