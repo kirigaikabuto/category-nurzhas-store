aa0f19540afc7133d0326580d63ad0fe510519fa
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-11-23 22:10:55 +0600

    Feat:change calc

--- order_service.go
+++ order_service.go
@@ -34,17 +34,9 @@ func (o *orderService) CreateOrder(cmd *CreateOrderCommand) (*Order, error) {
 	order.Length = cmd.Length
 	message := ""
 	message += fmt.Sprint("<pre><b>Калькулятор</b></pre>\n")
-	message += fmt.Sprintf("<pre><b>%s</b></pre>\n", "Размер здания")
-	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Тип здания", cmd.BuildingType)
-	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Ширина", cmd.Width)
-	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Высота", cmd.Height)
-	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Длина", cmd.Length)
-	message += fmt.Sprintf("<pre><b>%s</b></pre>\n", "Тип панели")
 	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Тип", cmd.PanelType)
 	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Утеплитель", cmd.InsulationType)
 	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Толщина панели", cmd.PanelDepth)
-	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Вид раскладки", cmd.LayoutType)
-	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Ширина панели", cmd.PanelWidth)
 	message += fmt.Sprintf("<pre>%s:<b>%s</b></pre>\n", "Цвет панели", cmd.Color)
 	err := o.telegramService.SendTelegramMessage("", message, "HTML")
 	if err != nil {
